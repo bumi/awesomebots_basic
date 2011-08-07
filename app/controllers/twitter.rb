a482d9e9046de91a72c74332adee17d1e7cc7150
Michael Bumann <michael@railslove.com>
2011-08-08 01:29:02 +0200

    oops

--- app/controllers/twitter.rb
+++ app/controllers/twitter.rb
@@ -7,7 +7,7 @@ Base.controllers :twitter do
   # regex: /^\/lto (.+)/
   post :lto, :provides => [:json] do
     id = Twitter.user_timeline(@message.twitter_name).first.id
-    tweet = "http://twitter.com/#{twitter_name}/status/#{id}"
+    tweet = "http://twitter.com/#{@message.twitter_name}/status/#{id}"
     {:type => "TweetMessage", :body => tweet}.to_json
   end
 end
